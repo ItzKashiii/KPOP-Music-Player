7d4a56bb81aa7c9d1922278620738f64eac4f1de
ItzKashiii <150389478+ItzKashiii@users.noreply.github.com>
2023-11-11 23:29:23 +0800

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,419 @@
+'use strict';
+
+
+
+/**
+ * all music information
+ */
+
+const musicData = [
+  {
+    backgroundImage: "https://i1.sndcdn.com/artworks-HCzFLZBG3tpmXFSL-Qqe5HQ-t500x500.jpg",
+    posterUrl: "https://i1.sndcdn.com/artworks-HCzFLZBG3tpmXFSL-Qqe5HQ-t500x500.jpg",
+    title: "Baddie",
+    album: "I've Mine",
+    year: 2023,
+    artist: "IVE",
+    musicPath: "./assets/music/baddie.mp3",
+  },
+  {
+    backgroundImage: "https://upload.wikimedia.org/wikipedia/en/7/73/STAYC_-_Teenfresh.png",
+    posterUrl: "https://upload.wikimedia.org/wikipedia/en/7/73/STAYC_-_Teenfresh.png",
+    title: "Bubble",
+    album: "Teenfresh",
+    year: 2023,
+    artist: "STAYC",
+    musicPath: "./assets/music/bubble.mp3",
+  },
+  {
+    backgroundImage: "https://preview.redd.it/xg-the-1st-mini-album-new-dna-trailer-teaser-images-hesonoo-v0-m4i5naaou2eb1.jpg?width=1080&format=pjpg&auto=webp&s=6430fe5daa8c52b81e4bf40fe0abfb64d571be3e",
+    posterUrl: "https://preview.redd.it/xg-the-1st-mini-album-new-dna-trailer-teaser-images-hesonoo-v0-m4i5naaou2eb1.jpg?width=1080&format=pjpg&auto=webp&s=6430fe5daa8c52b81e4bf40fe0abfb64d571be3e",
+    title: "NEW DANCE",
+    album: "NEW DNA",
+    year: 2023,
+    artist: "XG",
+    musicPath: "./assets/music/new_dance.mp3",
+  },
+  {
+    backgroundImage: "https://upload.wikimedia.org/wikipedia/en/e/ee/NewJeans_-_Get_Up.png",
+    posterUrl: "https://upload.wikimedia.org/wikipedia/en/e/ee/NewJeans_-_Get_Up.png",
+    title: "Super Shy",
+    album: "New Jeans",
+    year: 2023,
+    artist: "Get Up",
+    musicPath: "./assets/music/super_shy.mp3",
+  },
+  {
+    backgroundImage: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/99c08eac-1777-4459-81b0-341be2da5190/dekakd3-0f09ac74-bd35-4354-a5d2-dbf11544b219.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzk5YzA4ZWFjLTE3NzctNDQ1OS04MWIwLTM0MWJlMmRhNTE5MFwvZGVrYWtkMy0wZjA5YWM3NC1iZDM1LTQzNTQtYTVkMi1kYmYxMTU0NGIyMTkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.jUZ8nWzWLNXXCf6JdZJ4QzUWBcm5hx0HthzhFTuoyEI",
+    posterUrl: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/99c08eac-1777-4459-81b0-341be2da5190/dekakd3-0f09ac74-bd35-4354-a5d2-dbf11544b219.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzk5YzA4ZWFjLTE3NzctNDQ1OS04MWIwLTM0MWJlMmRhNTE5MFwvZGVrYWtkMy0wZjA5YWM3NC1iZDM1LTQzNTQtYTVkMi1kYmYxMTU0NGIyMTkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.jUZ8nWzWLNXXCf6JdZJ4QzUWBcm5hx0HthzhFTuoyEI",
+    title: "Feel Good (SECRET CODE)",
+    album: "My Little Society",
+    year: 2020,
+    artist: "Fromis_9",
+    musicPath: "./assets/music/feel_good.mp3",
+  },
+  {
+    backgroundImage: "https://www.kpopusaonline.com/wp-content/uploads/2019/11/228.1.jpg",
+    posterUrl: "https://www.kpopusaonline.com/wp-content/uploads/2019/11/228.1.jpg",
+    title: "Knock Knock",
+    album: "Twicecoaster",
+    year: 2017,
+    artist: "TWICE",
+    musicPath: "./assets/music/knock_knock.mp3",
+  },
+];
+
+
+
+/**
+ * add eventListnere on all elements that are passed
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+
+
+/**
+ * PLAYLIST
+ * 
+ * add all music in playlist, from 'musicData'
+ */
+
+const playlist = document.querySelector("[data-music-list]");
+
+for (let i = 0, len = musicData.length; i < len; i++) {
+  playlist.innerHTML += `
+  <li>
+    <button class="music-item ${i === 0 ? "playing" : ""}" data-playlist-toggler data-playlist-item="${i}">
+      <img src="${musicData[i].posterUrl}" width="800" height="800" alt="${musicData[i].title} Album Poster"
+        class="img-cover">
+
+      <div class="item-icon">
+        <span class="material-symbols-rounded">equalizer</span>
+      </div>
+    </button>
+  </li>
+  `;
+}
+
+
+
+/**
+ * PLAYLIST MODAL SIDEBAR TOGGLE
+ * 
+ * show 'playlist' modal sidebar when click on playlist button in top app bar
+ * and hide when click on overlay or any playlist-item
+ */
+
+const playlistSideModal = document.querySelector("[data-playlist]");
+const playlistTogglers = document.querySelectorAll("[data-playlist-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const togglePlaylist = function () {
+  playlistSideModal.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("modalActive");
+}
+
+addEventOnElements(playlistTogglers, "click", togglePlaylist);
+
+
+
+/**
+ * PLAYLIST ITEM
+ * 
+ * remove active state from last time played music
+ * and add active state in clicked music
+ */
+
+const playlistItems = document.querySelectorAll("[data-playlist-item]");
+
+let currentMusic = 0;
+let lastPlayedMusic = 0;
+
+const changePlaylistItem = function () {
+  playlistItems[lastPlayedMusic].classList.remove("playing");
+  playlistItems[currentMusic].classList.add("playing");
+}
+
+addEventOnElements(playlistItems, "click", function () {
+  lastPlayedMusic = currentMusic;
+  currentMusic = Number(this.dataset.playlistItem);
+  changePlaylistItem();
+});
+
+
+
+/**
+ * PLAYER
+ * 
+ * change all visual information on player, based on current music
+ */
+
+const playerBanner = document.querySelector("[data-player-banner]");
+const playerTitle = document.querySelector("[data-title]");
+const playerAlbum = document.querySelector("[data-album]");
+const playerYear = document.querySelector("[data-year]");
+const playerArtist = document.querySelector("[data-artist]");
+
+const audioSource = new Audio(musicData[currentMusic].musicPath);
+
+const changePlayerInfo = function () {
+  playerBanner.src = musicData[currentMusic].posterUrl;
+  playerBanner.setAttribute("alt", `${musicData[currentMusic].title} Album Poster`);
+  document.body.style.backgroundImage = `url(${musicData[currentMusic].backgroundImage})`;
+  playerTitle.textContent = musicData[currentMusic].title;
+  playerAlbum.textContent = musicData[currentMusic].album;
+  playerYear.textContent = musicData[currentMusic].year;
+  playerArtist.textContent = musicData[currentMusic].artist;
+
+  audioSource.src = musicData[currentMusic].musicPath;
+
+  audioSource.addEventListener("loadeddata", updateDuration);
+  playMusic();
+}
+
+addEventOnElements(playlistItems, "click", changePlayerInfo);
+
+/** update player duration */
+const playerDuration = document.querySelector("[data-duration]");
+const playerSeekRange = document.querySelector("[data-seek]");
+
+/** pass seconds and get timcode formate */
+const getTimecode = function (duration) {
+  const minutes = Math.floor(duration / 60);
+  const seconds = Math.ceil(duration - (minutes * 60));
+  const timecode = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  return timecode;
+}
+
+const updateDuration = function () {
+  playerSeekRange.max = Math.ceil(audioSource.duration);
+  playerDuration.textContent = getTimecode(Number(playerSeekRange.max));
+}
+
+audioSource.addEventListener("loadeddata", updateDuration);
+
+
+
+/**
+ * PLAY MUSIC
+ * 
+ * play and pause music when click on play button
+ */
+
+const playBtn = document.querySelector("[data-play-btn]");
+
+let playInterval;
+
+const playMusic = function () {
+  if (audioSource.paused) {
+    audioSource.play();
+    playBtn.classList.add("active");
+    playInterval = setInterval(updateRunningTime, 500);
+  } else {
+    audioSource.pause();
+    playBtn.classList.remove("active");
+    clearInterval(playInterval);
+  }
+}
+
+playBtn.addEventListener("click", playMusic);
+
+
+/** update running time while playing music */
+
+const playerRunningTime = document.querySelector("[data-running-time");
+
+const updateRunningTime = function () {
+  playerSeekRange.value = audioSource.currentTime;
+  playerRunningTime.textContent = getTimecode(audioSource.currentTime);
+
+  updateRangeFill();
+  isMusicEnd();
+}
+
+
+
+/**
+ * RANGE FILL WIDTH
+ * 
+ * change 'rangeFill' width, while changing range value
+ */
+
+const ranges = document.querySelectorAll("[data-range]");
+const rangeFill = document.querySelector("[data-range-fill]");
+
+const updateRangeFill = function () {
+  let element = this || ranges[0];
+
+  const rangeValue = (element.value / element.max) * 100;
+  element.nextElementSibling.style.width = `${rangeValue}%`;
+}
+
+addEventOnElements(ranges, "input", updateRangeFill);
+
+
+
+/**
+ * SEEK MUSIC
+ * 
+ * seek music while changing player seek range
+ */
+
+const seek = function () {
+  audioSource.currentTime = playerSeekRange.value;
+  playerRunningTime.textContent = getTimecode(playerSeekRange.value);
+}
+
+playerSeekRange.addEventListener("input", seek);
+
+
+
+/**
+ * END MUSIC
+ */
+
+const isMusicEnd = function () {
+  if (audioSource.ended) {
+    playBtn.classList.remove("active");
+    audioSource.currentTime = 0;
+    playerSeekRange.value = audioSource.currentTime;
+    playerRunningTime.textContent = getTimecode(audioSource.currentTime);
+    updateRangeFill();
+  }
+}
+
+
+
+/**
+ * SKIP TO NEXT MUSIC
+ */
+
+const playerSkipNextBtn = document.querySelector("[data-skip-next]");
+
+const skipNext = function () {
+  lastPlayedMusic = currentMusic;
+
+  if (isShuffled) {
+    shuffleMusic();
+  } else {
+    currentMusic >= musicData.length - 1 ? currentMusic = 0 : currentMusic++;
+  }
+
+  changePlayerInfo();
+  changePlaylistItem();
+}
+
+playerSkipNextBtn.addEventListener("click", skipNext);
+
+
+
+/**
+ * SKIP TO PREVIOUS MUSIC
+ */
+
+const playerSkipPrevBtn = document.querySelector("[data-skip-prev]");
+
+const skipPrev = function () {
+  lastPlayedMusic = currentMusic;
+
+  if (isShuffled) {
+    shuffleMusic();
+  } else {
+    currentMusic <= 0 ? currentMusic = musicData.length - 1 : currentMusic--;
+  }
+
+  changePlayerInfo();
+  changePlaylistItem();
+}
+
+playerSkipPrevBtn.addEventListener("click", skipPrev);
+
+
+
+/**
+ * SHUFFLE MUSIC
+ */
+
+/** get random number for shuffle */
+const getRandomMusic = () => Math.floor(Math.random() * musicData.length);
+
+const shuffleMusic = () => currentMusic = getRandomMusic();
+
+const playerShuffleBtn = document.querySelector("[data-shuffle]");
+let isShuffled = false;
+
+const shuffle = function () {
+  playerShuffleBtn.classList.toggle("active");
+
+  isShuffled = isShuffled ? false : true;
+}
+
+playerShuffleBtn.addEventListener("click", shuffle);
+
+
+
+/**
+ * REPEAT MUSIC
+ */
+
+const playerRepeatBtn = document.querySelector("[data-repeat]");
+
+const repeat = function () {
+  if (!audioSource.loop) {
+    audioSource.loop = true;
+    this.classList.add("active");
+  } else {
+    audioSource.loop = false;
+    this.classList.remove("active");
+  }
+}
+
+playerRepeatBtn.addEventListener("click", repeat);
+
+
+
+/**
+ * MUSIC VOLUME
+ * 
+ * increase or decrease music volume when change the volume range
+ */
+
+const playerVolumeRange = document.querySelector("[data-volume]");
+const playerVolumeBtn = document.querySelector("[data-volume-btn]");
+
+const changeVolume = function () {
+  audioSource.volume = playerVolumeRange.value;
+  audioSource.muted = false;
+
+  if (audioSource.volume <= 0.1) {
+    playerVolumeBtn.children[0].textContent = "volume_mute";
+  } else if (audioSource.volume <= 0.5) {
+    playerVolumeBtn.children[0].textContent = "volume_down";
+  } else {
+    playerVolumeBtn.children[0].textContent = "volume_up";
+  }
+}
+
+playerVolumeRange.addEventListener("input", changeVolume);
+
+
+/**
+ * MUTE MUSIC
+ */
+
+const muteVolume = function () {
+  if (!audioSource.muted) {
+    audioSource.muted = true;
+    playerVolumeBtn.children[0].textContent = "volume_off";
+  } else {
+    changeVolume();
+  }
+}
+
+playerVolumeBtn.addEventListener("click", muteVolume);
